b7fcba4a4387c8c9eb0b4eccab53c3a87aabc348
mirandadayadkins <mirandaday16@gmail.com>
2020-11-29 18:10:16 -0500

    friends activity layout

--- app/src/main/java/edu/neu/junglee/LoginActivity.kt
+++ app/src/main/java/edu/neu/junglee/LoginActivity.kt
@@ -3,7 +3,7 @@ package edu.neu.junglee
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
